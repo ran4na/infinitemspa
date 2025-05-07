23a6db26f627d2027647b15f7410a5ed3225540e
ghopal <wogobogo@protonmail.com>
2025-05-06 20:06:16 -0700

    Add image tag implementation and turn off auto link parsing

--- src/bbcode_custom.py
+++ src/bbcode_custom.py
@@ -1,6 +1,6 @@
 import bbcode
 
-parser = bbcode.Parser()
+parser = bbcode.Parser(replace_links=False)
 
 # Non-collapsing pesterlog tag.
 def render_pesterlog(tag_name, value, options, parent, context):
@@ -10,5 +10,11 @@ def render_pesterlog(tag_name, value, options, parent, context):
 def render_size(tag_name, value, options, parent, context):
     return f'<span class="size">{value}</span>'
 
+#placeholder
+def render_image(tag_name, value, options, parent, context):
+    return f'<img src="{value}" style="max-width: 540px"/>'
+
+
 parser.add_formatter('pesterlog', render_pesterlog)
 parser.add_formatter('size', render_size)
+parser.add_formatter('img', render_image)
